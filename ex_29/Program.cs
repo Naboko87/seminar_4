104e74dadf7ddc16bdf718b20f9a5ee46ce375a3
Kseniya <kseniya@MacBook-Pro-Kseniya.local>
2022-11-11 01:21:28 +0300

    ex_29

---
+++ ex_29/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("write number");
+//int number_1 = Convert.ToInt32(Console.ReadLine());
+string input = Console.ReadLine();
+string[] numbers = input.Split(",");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i]); 
+}
