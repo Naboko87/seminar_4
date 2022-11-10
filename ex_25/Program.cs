0741c6184dde3431d587eb82b5ed65a72d1622c2
Kseniya <kseniya@MacBook-Pro-Kseniya.local>
2022-11-10 21:34:03 +0300

    ex_25

---
+++ ex_25/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, write number for A!");
+int number_a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Hello, write number for B!");
+int number_b = Convert.ToInt32(Console.ReadLine());
+
+Exponentiate(number_a, number_b);
+
+void Exponentiate(int x, int stepen){
+    Console.WriteLine("a^b = " + Math.Pow(x, stepen));
+}
