1c1be3a892ce61d7cb7374819fce030efe044090
Kseniya <kseniya@MacBook-Pro-Kseniya.local>
2022-11-11 00:02:25 +0300

    ex_27

---
+++ ex_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, write number");
+string number = Console.ReadLine();
+
+sumNumber(number);
+
+void sumNumber(string a){
+int sum = 0;
+    if (a != "")
+    { 
+        for (int i = 0; i <= a.Length-1; i++)
+        {
+            int b = Convert.ToInt32(new string(a[i], 1));
+            sum = sum + b;
+        }   
+    }
+    Console.WriteLine(sum);
+}
